074a262179a616b0a1cd9cb51adb74f02bb9e4b4
LUIS ESPINOSA <espinosabouvy@gmail.com>
2016-09-22 23:42:31 -0500

    Final semana 3 courso DataCleaning

--- main_commands.R
+++ main_commands.R
@@ -12,6 +12,7 @@ if (!file.exists("data")){
 download.file (siempre crear una variable con la fecha en que se descarga
 Excel - library("openxlsx")
 var <- read.xlsx("archivo", sheets = 1, header=TRUE, colIndex = 2:3, rowIndex = 6:12)
+cuando se descarga en windows, tratar de usar mode = "wb" para evitar que se corrompan
 
 #XML Y HTML
 library(XML)
@@ -64,7 +65,6 @@ xpathSApply(parsedHtml, "//title", xmlValue)
 
 #API'S
 #revisar development en face, twitter, google, etc.
-"GitHub - crear development account - generar una aplicacion y obtener "
 
 #others
 hay packages para todo, específico, revisar 
@@ -73,8 +73,41 @@ leer .jpg, bitmaps, png, mp3, tuneR  (revisar, deber ser interesante)
 
 
 
+#DPLYR
+library(dplyr)
+var <- tbl_df(data)  #carga en el formato de dplyr la informacion
+select #seleccionar desde tabla
+filter #filtrar por diferentes condiciones
+mutate #agregar columnas con calculos
+arrange #ordenar asc por default, desc para cambiar a descendente
+summarize #calculos de los totales o subgrupos
+group_by  #crea grupos, como sapply
+chaining # usar %>% para utilizar el objeto anterior como el objeto base de cálculo del siguiente
+#Ejemplo de chainning
+cran %>%
+     select(ip_id, country, package, size) %>%
+     mutate(size_mb = size / 2^20) %>%
+     filter(size_mb <= 0.5) %>%
+     arrange(desc(size_mb))
 
+#tidyr
+library(tidyr)
+library(readr)
+gather #se utiliza cuando se tienen columnas que no se consideran variables, por ejemplo, columna llamada grade, male, female
+"It's important to understand what each argument to gather() means. The data argument, students, gives the name of the
+| original dataset. The key and value arguments -- sex and count, respectively -- give the column names for our tidy
+| dataset. The final argument, -grade, says that we want to gather all columns EXCEPT the grade column (since grade is
+| already a proper column variable.)"
+separate #separa una columna que tenga 2 variables- ejm- estilo-material, lo separa en estilo y en material
+separate(data = res, col = sex_class, into = c("sex", "class"))  #default algo que no sea numerico o alpha para separar
 
+spread  ##crea columnas con cada uno de los datos unicos de los renglones de una columna
+parse_number #descompone una palabra en sus partes, por ejemplo "class5" regresa 5, existe date, string, etc.
 
+#MERGE
+merge #equivalente a un join en SQL, define las 2 tablas, by.x y by.y para dar el nombre de los ids a juntar
 
+#LUBRIDATE
+library(lubridate)
 
+     
